03ff4b421b2836a51d9db87cb10ea14960527777
Abhinav <github@itsabhinav.in>
2024-10-12 20:15:47 +0530

    feat: option to customize date range

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import ScrollableDatePicker from '$lib/ScrollableDatePicker.svelte';
+
+	$: selectedDate = '2012-12-11';
 </script>
 
 <h1>Welcome to your ScrollableDatePicker</h1>
 
 <b> ScrollableDatePicker example: </b>
 <div class="w-3/4">
-	<ScrollableDatePicker selectedDate="2023-01-23" rowsShown={5} separatorWidth={4} />
+	<ScrollableDatePicker {selectedDate} rowsShown={5} separatorWidth={4} startFrom="2014-10-11" />
 </div>
